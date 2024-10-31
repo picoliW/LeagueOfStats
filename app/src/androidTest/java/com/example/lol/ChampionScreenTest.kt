7ee9c03024f403899519eca73a3dfd69a96a71f0
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-10-31 16:21:00 -0300

    test7

--- app/src/androidTest/java/com/example/lol/ChampionScreenTest.kt
+++ app/src/androidTest/java/com/example/lol/ChampionScreenTest.kt
@@ -164,14 +164,6 @@ class ChampionsScreenTest {
         assertTrue(filteredChampions.isEmpty())
     }
 
-    @Test
-    fun testTranslationFunction() {
-        val originalText = "the Nine-Tailed Fox"
-        val translatedText = translateText(originalText, "pt")
-
-        assertNotNull(translatedText)
-        assertEquals("a raposa de nove caudas", translatedText)
-    }
 
 
 
